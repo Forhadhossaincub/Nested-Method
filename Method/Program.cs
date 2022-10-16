e080784f1ae102930fc6ae620230a84f6341044b
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 12:58:55 +0600

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NewMethod();
+
+            M();
+
+            Console.ReadKey();
+        }
+        public static void NewMethod()
+        {
+            M();
+        }
+
+        public static void M()
+        {
+            Console.WriteLine("This is M method.");
+        }
+
+        
+    }
+}
